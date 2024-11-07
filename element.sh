e40c41a9275b2fc73e9e899c361aa90e417ec0d7
Yamini Srinivasan <yaminianitha22@gmail.com>
2024-11-07 05:23:24 +0000

    feat: add argument check to element.sh

--- element.sh
+++ element.sh
@@ -2,3 +2,4 @@
 echo 'Script running...'
 echo "Please provide an element as an argument."
 echo "Please provide an element as an argument."
+if [[ -z $1 ]]; then echo "Please provide an element as an argument."; fi
