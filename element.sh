9efb3d01a5cfab8aacd2b7112c1b1fa5ff5c478c
Yamini Srinivasan <yaminianitha22@gmail.com>
2024-11-07 05:40:38 +0000

    feat: add argument check to element.sh

--- element.sh
+++ element.sh
@@ -5,3 +5,4 @@ echo "Please provide an element as an argument."
 if [[ -z $1 ]]; then echo "Please provide an element as an argument."; fi
 if [[ $1 == "H" ]]; then echo "The element with atomic number 1 is Hydrogen (H)."; fi
 echo "Please provide an element as an argument."
+if [[ -z $1 ]]; then echo "Please provide an element as an argument."; fi
